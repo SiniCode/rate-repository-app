097f89115cdd3005c3345aa9d69729089a69e91b
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-07-21 16:48:55 +0300

    Make app bar scrollable

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import theme from '../theme';
 import AppBarTab from './AppBarTab';
@@ -9,6 +9,8 @@ const styles = StyleSheet.create({
 		padding: 20,
 		height: 80,
 		backgroundColor: theme.colors.backgroundDark,
+	},
+	scrollView: {
 		display: 'flex',
 		flexDirection: 'row',
 		alignItems: 'flex-end',
@@ -23,8 +25,10 @@ const styles = StyleSheet.create({
 const AppBar = () => {
 	return (
 		<View style={styles.container}>
-			<AppBarTab text='Repositories' route='/' style={styles.item} />
-			<AppBarTab text='Sign in' route='/sign-in' style={styles.item} />
+			<ScrollView horizontal contentContainerStyle={styles.scrollView}>
+				<AppBarTab text='Repositories' route='/' style={styles.item} />
+				<AppBarTab text='Sign in' route='/sign-in' style={styles.item} />
+			</ScrollView>
 		</View>
 	);
 };
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -43,7 +43,7 @@ const styles = StyleSheet.create({
 	},
 	description: {
 		flex: 1,
-		width: 250,
+		maxWidth: 260,
 	},
 });
 
