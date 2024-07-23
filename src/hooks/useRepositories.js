850e2cac84e756167fe2f9fe1cc2b04083c7c073
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-07-23 19:44:46 +0300

    Fetch repositories with graphql

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,26 +1,24 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-	const [repositories, setRepositories] = useState();
-	const [loading, setLoading] = useState(false);
+	const { data, error, loading } = useQuery(GET_REPOSITORIES, {
+		fetchPolicy: 'cache-and-network',
+	});
 
-	const fetchRepositories = async () => {
-		setLoading(true);
+	if (error) {
+		console.log(error.message);
+	}
 
-		const response = await fetch(
-			'http://192.168.101.102:5000/api/repositories'
-		);
-		const json = await response.json();
+	if (loading) {
+		console.log('loading');
+		return [];
+	}
 
-		setLoading(false);
-		setRepositories(json);
-	};
+	const repositories = data.repositories;
 
-	useEffect(() => {
-		fetchRepositories();
-	}, []);
-
-	return { repositories, loading, refetch: fetchRepositories };
+	return { repositories, loading };
 };
 
 export default useRepositories;
