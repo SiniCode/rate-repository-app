5d6d5489c384b489df3dbaff707bdc163bc96250
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-08-19 17:08:56 +0300

    Add sorting feature for repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,49 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-native';
 import { FlatList, View, Pressable, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
 import useRepositories from '../hooks/useRepositories';
 import RepositoryItem from './RepositoryItem';
+import theme from '../theme';
 
 const styles = StyleSheet.create({
 	separator: {
 		height: 10,
 	},
+	picker: {
+		padding: 30,
+		paddingLeft: 20,
+		maxWidth: 460,
+		color: theme.colors.textPrimary,
+		fontFamily: theme.fonts.main,
+		fontSize: theme.fontSizes.subheading,
+		backgroundColor: theme.colors.backgroundLight,
+		borderWidth: 0,
+	},
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, navigate }) => {
+const OrderPicker = ({ order, setOrder }) => {
+	return (
+		<Picker
+			selectedValue={order}
+			onValueChange={(itemValue) => setOrder(itemValue)}
+			style={styles.picker}
+		>
+			<Picker.Item label='Latest repositories' value='latest' />
+			<Picker.Item label='Highest rated repositories' value='highest' />
+			<Picker.Item label='Lowest rated repositories' value='lowest' />
+		</Picker>
+	);
+};
+
+export const RepositoryListContainer = ({
+	repositories,
+	navigate,
+	order,
+	setOrder,
+}) => {
 	const repositoryNodes = repositories
 		? repositories.edges.map((edge) => edge.node)
 		: [];
@@ -19,6 +51,7 @@ export const RepositoryListContainer = ({ repositories, navigate }) => {
 	return (
 		<FlatList
 			data={repositoryNodes}
+			ListHeaderComponent={<OrderPicker order={order} setOrder={setOrder} />}
 			ItemSeparatorComponent={ItemSeparator}
 			renderItem={({ item }) => (
 				<Pressable onPress={() => navigate(`/repository/${item.id}`)}>
@@ -30,11 +63,20 @@ export const RepositoryListContainer = ({ repositories, navigate }) => {
 };
 
 const RepositoryList = () => {
-	const { repositories } = useRepositories();
+	const [order, setOrder] = useState('latest');
+	const { repositories } = useRepositories({
+		orderBy: order === 'latest' ? 'CREATED_AT' : 'RATING_AVERAGE',
+		orderDirection: order === 'lowest' ? 'ASC' : 'DESC',
+	});
 	const navigate = useNavigate();
 
 	return (
-		<RepositoryListContainer repositories={repositories} navigate={navigate} />
+		<RepositoryListContainer
+			repositories={repositories}
+			navigate={navigate}
+			order={order}
+			setOrder={setOrder}
+		/>
 	);
 };
 
