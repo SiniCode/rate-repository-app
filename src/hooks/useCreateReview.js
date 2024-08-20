07066246a92d4f73135396527df4a95fd92c1cff
Sini Arkko <83418050+SiniCode@users.noreply.github.com>
2024-08-20 15:59:31 +0300

    Refetch user reviews when new review is added

--- src/hooks/useCreateReview.js
+++ src/hooks/useCreateReview.js
@@ -1,8 +1,13 @@
 import { useMutation } from '@apollo/client';
 import { CREATE_REVIEW } from '../graphql/mutations';
+import { CURRENT_USER } from '../graphql/queries';
 
 const useCreateReview = () => {
-	const [mutate, result] = useMutation(CREATE_REVIEW);
+	const [mutate, result] = useMutation(CREATE_REVIEW, {
+		refetchQueries: [
+			{ query: CURRENT_USER, variables: { includeReviews: true } },
+		],
+	});
 
 	const createReview = async ({ ownerName, repositoryName, rating, text }) => {
 		const { data } = await mutate({
